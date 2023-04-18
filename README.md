157b2de5eff3c26d14b7811c33a3ac55f43ee0b6
Pablo Alba <pablo.alba@kaleidos.net>
2023-04-18 11:31:02 +0200

    Web interface

--- README.md
+++ README.md
@@ -6,10 +6,23 @@ python -m venv venv
 source venv/bin/activate
 pip install git+https://github.com/ShoneGK/ChatterPy
 python3 -m spacy download en_core_web_sm
+pip install chatterbot-corpus flask flask-socketio
+pip install --upgrade PyYaml
 ```
+
+
+# Train
+
+```
+python penny.py train
+```
+
 
 # Run
 
 ```
 python penny.py
 ```
+
+The web interface will be at http://127.0.0.1:5000/
+
--- penny.py
+++ penny.py
@@ -2,44 +2,63 @@ from chatterbot import ChatBot
 from chatterbot.comparisons import JaccardSimilarity, LevenshteinDistance
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+import sys
 
 
+def init_chatbot():
+    chatbot = ChatBot(
+        'Penny',
+        read_only=True,
+        preprocessors=['chatterbot.preprocessors.clean_whitespace'],
+        statement_comparison_function=JaccardSimilarity,
+        #statement_comparison_function=LevenshteinDistance,
+        storage_adapter='chatterbot.storage.SQLStorageAdapter',
+        logic_adapters=[
+            {
+                'import_path': 'chatterbot.logic.BestMatch',
+                'default_response': 'I am sorry, but I do not understand.',
+                'maximum_similarity_threshold': 0.8
+            }
+        ],
+        database_uri='sqlite:///database.sqlite3'
+    )
+    return chatbot
 
-chatbot = ChatBot(
-    'Penny',
-    read_only=True,
-    preprocessors=['chatterbot.preprocessors.clean_whitespace'],
-    statement_comparison_function=JaccardSimilarity,
-    #statement_comparison_function=LevenshteinDistance,
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand.',
-            'maximum_similarity_threshold': 0.8
-        }
-    ],
-    database_uri='sqlite:///database.sqlite3'
-)
+def train_chatbot(chatbot):
+    corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+    corpus_trainer.train('data/greetings.yml')
+    corpus_trainer.train('data/faq.yml')
+    corpus_trainer.train('data/shortcuts.yml')
+    corpus_trainer.train('data/user_guide.yml')
 
 
-corpus_trainer = ChatterBotCorpusTrainer(chatbot)
-corpus_trainer.train('data/greetings.yml')
-corpus_trainer.train('data/faq.yml')
-corpus_trainer.train('data/shortcuts.yml')
-corpus_trainer.train('data/user_guide.yml')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+chatbot = init_chatbot()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-exit_conditions = (":q", "quit", "exit")
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    response = str(chatbot.get_response(message))
+    emit('response', '[P]: ' + response, broadcast=True)
 
-while True:
-
-    query = input("> ")
-
-    if query in exit_conditions:
-
-        break
-
+def main(argv):
+    print (argv)
+    if len(argv) == 1 and argv[0] == 'train':
+        train_chatbot(chatbot)
+        print("Chatbot trained!")
+        exit(0)
     else:
+        socketio.run(app, debug=True)
 
-        print(f"[P] {chatbot.get_response(query)}")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
