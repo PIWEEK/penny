4d498892bac153d816466037a3e7625b6a8080cd
Pablo Alba <pablo.alba@kaleidos.net>
2023-04-18 10:21:46 +0200

    Migrate to yml

--- penny.py
+++ penny.py
@@ -1,31 +1,33 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-#from chatterbot.comparisons import jaccard_similarity
-#from chatterbot.comparisons import levenshtein_distance
-from data import faq, shortcuts
+from chatterbot.comparisons import JaccardSimilarity, LevenshteinDistance
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
 
 
 chatbot = ChatBot(
     'Penny',
     read_only=True,
     preprocessors=['chatterbot.preprocessors.clean_whitespace'],
-    #statement_comparison_function=jaccard_similarity, #levenshtein_distance
+    statement_comparison_function=JaccardSimilarity,
+    #statement_comparison_function=LevenshteinDistance,
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I am sorry, but I do not understand.',
-            'maximum_similarity_threshold': 0.60
+            'maximum_similarity_threshold': 0.8
         }
     ],
     database_uri='sqlite:///database.sqlite3'
 )
 
 
-trainer = ListTrainer(chatbot)
-
-trainer.train(faq)
-trainer.train(shortcuts)
+corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+corpus_trainer.train('data/greetings.yml')
+corpus_trainer.train('data/faq.yml')
+corpus_trainer.train('data/shortcuts.yml')
+corpus_trainer.train('data/user_guide.yml')
 
 
 exit_conditions = (":q", "quit", "exit")
